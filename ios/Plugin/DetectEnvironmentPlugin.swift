cf7bd5e54f18cae8fd13d96205f23ae76c98f93e
Stepan Kotov <rediska1114@gmail.com>
2023-07-06 02:15:30 +0600

    refactor: rename deprecated methods

--- ios/Plugin/DetectEnvironmentPlugin.swift
+++ ios/Plugin/DetectEnvironmentPlugin.swift
@@ -10,37 +10,37 @@ public class DetectEnvironmentPlugin: CAPPlugin {
     private let implementation = DetectEnvironment()
 
     @objc func isDebug(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isDebug(),
         ])
     }
 
     @objc func isTestFlight(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isTestFlight(),
         ])
     }
 
     @objc func isAdHoc(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isAdHoc(),
         ])
     }
 
     @objc func isSimulator(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isSimulator(),
         ])
     }
 
     @objc func isAppStore(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isAppStore(),
         ])
     }
 
     @objc func isSandbox(_ call: CAPPluginCall) {
-        call.success([
+        call.resolve([
             "value": implementation.isSandbox(),
         ])
     }
